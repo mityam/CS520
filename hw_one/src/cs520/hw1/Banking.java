36fc4ab93c9bfafb4e602a9569ee3e686d476847
mityam <mityam@yahoo.com>
2020-09-24 23:27:39 -0400

    1

--- hw_one/src/cs520/hw1/Banking.java
+++ hw_one/src/cs520/hw1/Banking.java
@@ -1,5 +1,6 @@
 package cs520.hw1;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 
@@ -7,8 +8,15 @@ public class Banking {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		JFrame f=new JFrame();
 		
 		String input = JOptionPane.showInputDialog("Enter amount in dollars");
+		int hundreds = Integer.parseInt(input);
+		
+		hundreds = (hundreds - hundreds%100)/100;
+		
+		JOptionPane.showMessageDialog(f, hundreds);
+		
 		System.exit(0); 
 		
 	}
