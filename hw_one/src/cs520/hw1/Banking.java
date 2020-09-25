1f315c2c8c6497abc5f20c993dc9bc8d676e8dfe
mityam <mityam@yahoo.com>
2020-09-25 00:03:06 -0400

    1

--- hw_one/src/cs520/hw1/Banking.java
+++ hw_one/src/cs520/hw1/Banking.java
@@ -11,11 +11,40 @@ public class Banking {
 		JFrame f=new JFrame();
 		
 		String input = JOptionPane.showInputDialog("Enter amount in dollars");
-		int hundreds = Integer.parseInt(input);
+		int amount = Integer.parseInt(input);
+		System.out.println("Requested Amount = " + amount);
 		
-		hundreds = (hundreds - hundreds%100)/100;
-		
-		JOptionPane.showMessageDialog(f, hundreds);
+		int hundreds, fifties, twenties, tens, fives, twos, ones;
+		int remainingAmount;
+		remainingAmount = amount%100;
+		hundreds = (amount - remainingAmount)/100;
+		System.out.println("Hundreds = " + hundreds + ", RemainingAmount = " + remainingAmount);
+				
+		remainingAmount = amount%50;
+		fifties = (amount - remainingAmount)/50;
+		System.out.println("Fifties = " + fifties + ", RemainingAmount = " + remainingAmount);
+				
+		remainingAmount = amount%20;
+		twenties = (amount - remainingAmount)/20;
+		System.out.println("Twenties = " + twenties + ", RemainingAmount = " + remainingAmount);
+				
+		remainingAmount = amount%10;
+		tens = (amount - remainingAmount)/10;
+		System.out.println("Tens = " + tens + ", RemainingAmount = " + remainingAmount);
+				
+		remainingAmount = amount%5;
+		fives = (amount - remainingAmount)/5;
+		System.out.println("Fives = " + fives + ", RemainingAmount = " + remainingAmount);
+				
+		remainingAmount = amount%2;
+		twos = (amount - remainingAmount)/2;
+		System.out.println("Twos = " + twos + ", RemainingAmount = " + remainingAmount);
+				
+		remainingAmount = amount%1;
+		ones = (amount - remainingAmount)/1;
+		System.out.println("Ones = " + ones + ", RemainingAmount = " + remainingAmount);
+				
+		JOptionPane.showMessageDialog(f, "Hundreds=" + hundreds + "\nFifties=" + fifties + "\nTwenties=" + twenties + "\nTens=" + tens + "\nFives="+fives+"\nTwos=" + twos + "\nOnes="+ones);
 		
 		System.exit(0); 
 		
