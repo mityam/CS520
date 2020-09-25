1de187155a2365d46a9459d046a5a8f89cd8f138
mityam <mityam@yahoo.com>
2020-09-25 12:57:57 -0400

    triples

--- hw_one/src/cs520/hw1/Triples.java
+++ hw_one/src/cs520/hw1/Triples.java
@@ -4,6 +4,24 @@ public class Triples {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		for (int x=1; x <= 100; x++)
+		{
+			
+			for (int y=1; y <= 100; y++) 
+			{
+				
+				
+				
+			} //end of inner loop
+			
+			
+			
+		} //end of outer loop
+		
+		
+		
 
 	}
 
