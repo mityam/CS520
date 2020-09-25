106951f90733f2e08f40f9fd4845bd1d3cf6d6a4
mityam <mityam@yahoo.com>
2020-09-25 13:41:21 -0400

    4

--- hw_one/src/cs520/hw1/Triples.java
+++ hw_one/src/cs520/hw1/Triples.java
@@ -5,22 +5,25 @@ public class Triples {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int tripleCounter = 0;
 		System.out.println("Count     x     y     z");
-		for (int x=1; x <= 10; x++)
+		for (int x=1; x <= 20; x++)
 		{
 			
-			for (int y=1; y <= 10; y++) 
+			for (int y=1; y <= 20; y++) 
 			{
 				
 				double sum = Math.pow(x, 2) + Math.pow(y, 2);
 				int z =  (int) Math.sqrt(sum);
-				//System.out.println("x=" + x + "; y=" + y + "; sum =" + sum + "; z =" + z);
-				System.out.println(x*y + ")" + "        " + x + "     "+ y + "     " + z);
+				
+				if  ( Math.pow(z, 2) == sum )
+					{
+					tripleCounter = ++tripleCounter;
+					System.out.println(tripleCounter + ")" + "        " + x + "     "+ y + "     " + z + "  sum=" + sum );
+					
+					}
 				
 			} //end of inner loop
-			
-			
-			
 		} //end of outer loop
 		
 		
