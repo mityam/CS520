1e90099971462cae0635302c8fa255e809b64569
mityam <mityam@yahoo.com>
2020-09-25 13:20:26 -0400

    3

--- hw_one/src/cs520/hw1/Triples.java
+++ hw_one/src/cs520/hw1/Triples.java
@@ -6,16 +6,16 @@ public class Triples {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Count     x     y     z");
-		for (int x=1; x <= 100; x++)
+		for (int x=1; x <= 10; x++)
 		{
 			
-			for (int y=1; y <= 100; y++) 
+			for (int y=1; y <= 10; y++) 
 			{
 				
 				double sum = Math.pow(x, 2) + Math.pow(y, 2);
 				int z =  (int) Math.sqrt(sum);
 				//System.out.println("x=" + x + "; y=" + y + "; sum =" + sum + "; z =" + z);
-				System.out.println("1)" + x*y + "     " + x + "      "+ y + "     " + z);
+				System.out.println(x*y + ")" + "        " + x + "     "+ y + "     " + z);
 				
 			} //end of inner loop
 			
@@ -24,7 +24,7 @@ public class Triples {
 		} //end of outer loop
 		
 		
-		
+		System.exit(0);
 
 	}
 
