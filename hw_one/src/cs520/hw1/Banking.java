f42545ff0cc7852b168b1613ea93b4820d2f21b9
mityam <mityam@yahoo.com>
2020-09-24 23:04:40 -0400

    first java file for CS520

---
+++ hw_one/src/cs520/hw1/Banking.java
@@ -0,0 +1,17 @@
+package cs520.hw1;
+
+import javax.swing.JOptionPane;
+
+
+public class Banking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String input = JOptionPane.showInputDialog("Enter amount in dollars");
+		System.exit(0); 
+		
+	}
+
+	
+}
