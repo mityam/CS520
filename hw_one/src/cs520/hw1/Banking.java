ca83b2e6084ec65a8901d9f2df41e9b53682d2ae
mityam <mityam@yahoo.com>
2020-09-28 22:24:26 -0400

    all fixed

--- hw_one/src/cs520/hw1/Banking.java
+++ hw_one/src/cs520/hw1/Banking.java
@@ -7,50 +7,39 @@ import javax.swing.JOptionPane;
 public class Banking {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		JFrame f=new JFrame();
-		
 		//getting string input from user
 		String input = JOptionPane.showInputDialog("Enter amount in dollars");
 		int amount = Integer.parseInt(input);
 		System.out.println("Requested Amount = " + amount);
-		
 		//declaring all integers needed for the program
 		int hundreds, fifties, twenties, tens, fives, twos, ones;
 		int remainingAmount=0;
-		
 		//calculating number of hundreds in the amount
 		hundreds = (amount - remainingAmount)/100;
 		//calculating remainder of division by 100
 		remainingAmount = amount%100;
-		
 		//Printing out to console number of hundreds and remainder
 		System.out.println("Hundreds = " + hundreds + ", RemainingAmount = " + remainingAmount);
-		
 		fifties = remainingAmount/50;
 		remainingAmount = remainingAmount%50;
 		System.out.println("Fifties = " + fifties + ", RemainingAmount = " + remainingAmount);
-				
 		twenties = remainingAmount/20;
 		remainingAmount = remainingAmount%20;
 		System.out.println("Twenties = " + twenties + ", RemainingAmount = " + remainingAmount);
-				
 		tens = remainingAmount/10;
 		remainingAmount = remainingAmount%10;
 		System.out.println("Tens = " + tens + ", RemainingAmount = " + remainingAmount);
-				
 		fives = remainingAmount/5;
 		remainingAmount = remainingAmount%5;
 		System.out.println("Fives = " + fives + ", RemainingAmount = " + remainingAmount);
-				
 		twos = remainingAmount/2;
 		remainingAmount = remainingAmount%2;
 		System.out.println("Twos = " + twos + ", RemainingAmount = " + remainingAmount);
-				
 		ones = remainingAmount/1;
 		remainingAmount = remainingAmount%1;
 		System.out.println("Ones = " + ones + ", RemainingAmount = " + remainingAmount);
-				
 		//Displaying all results in dialog pane
 		JOptionPane.showMessageDialog(f, "Requested Amount=" + input + "\nHundreds=" + hundreds + "\nFifties="
 		+ fifties + "\nTwenties=" + twenties + "\nTens=" + tens + "\nFives="+fives+"\nTwos=" + twos + "\nOnes="+ones);
--- hw_one/src/cs520/hw1/Triples.java
+++ hw_one/src/cs520/hw1/Triples.java
@@ -6,23 +6,29 @@ import java.util.List;
 public class Triples {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int tripleCounter = 0;
-		System.out.println("Count     x     y     z");
+		System.out.println("Count   x    y    z");
 		for (int x = 1; x <= 100; x++)
 		{
-			for (int y = 1; y <= 100; y++) 
+			//for (int y = 1; y <= 100; y++)
+			//this below loop is optimal because it takes less iterations and if condition checks 
+			for (int y = x + 1; y <= 100; y++) 
 			{
 				double sum = Math.pow(x, 2) + Math.pow(y, 2);
 				int z =  (int) Math.sqrt(sum);
 					
 				// Checking here if the two integers x and y make an integer z based on x^2+y^2=z^2
 				//also checking if x and y are already on tripleList in reverse order
-				if  ( Math.pow(z, 2) == sum && (y > x))
+				//if  ( Math.pow(z, 2) == sum && (y > x))
+				
+				if  ( Math.pow(z, 2) == sum )	
 					{
 					tripleCounter = ++tripleCounter;
 					//Adding x,y and z values as a string to list to check later if xyzString string is in this list and avoid duplicates
-					System.out.println(tripleCounter + ")" + "        " + x + "     "+ y + "     " + z );
+					//System.out.println(tripleCounter + ")" + "        " + x + "     "+ y + "     " + z );
+					System.out.printf("%3d) %4d %4d %4d\n", tripleCounter, x, y, z );
+					
 					}
 			} //end of inner loop
 		} //end of outer loop
