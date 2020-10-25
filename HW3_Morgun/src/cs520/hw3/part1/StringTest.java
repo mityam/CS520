5b2f56d1a5fe65e06bc8ef22a025112b7b607103
mityamyahoo.com <mityamyahoo.com@DELL-T7810>
2020-10-25 10:33:05 -0400

    f

--- HW3_Morgun/src/cs520/hw3/part1/StringTest.java
+++ HW3_Morgun/src/cs520/hw3/part1/StringTest.java
@@ -9,9 +9,6 @@ public class StringTest {
 		try {
 		//part 1.a
 			String input = JOptionPane.showInputDialog("Please enter data using the format CourseId,CourseName,Tuition");
-			
-		
-		
 		//part 1.b
 		String trimmedString = input.trim();
 		//part 1.c
@@ -32,9 +29,7 @@ public class StringTest {
 		int tuition = Integer.parseInt(trimmedString.substring(secondComma+1));
 		System.out.println ("Regular Tuition $" + tuition
 				+ ", Discount Tuition $" + (tuition - tuition/4) );
-		
 		}
-		
 		//**part 1.i**//
 		//Cancel is clicked
 		catch (NullPointerException e)
@@ -42,26 +37,23 @@ public class StringTest {
 			System.out.println("Exception = " + e.toString());
 			System.out.println("User Exit");
 		}
-		//when only the course id is entered, when only the course id and the course name are entered
+		//when only the course id is entered or when only the course id and the course name are entered
 		catch (StringIndexOutOfBoundsException e)
 		{	
 			System.out.println("Exception = " + e.toString());
 			System.out.println("Missing user input");
 		}	
-		//when only the course id is entered, when only the course id and the course name are entered
+		//when tuition is not an integer
 		catch (NumberFormatException e)
 		{	
 			System.out.println("Exception = " + e.toString());
-			System.out.println("Tution can be only numeric integer");
+			System.out.println("Tution can only be an integer");
 		}	
-		
 		//other less specific exception not handled above
 		catch (Exception e)
 		{
 			System.out.println("General Exception");
 			System.out.println("e = " + e.toString());
 		}	
-		
 	}
-
 }
