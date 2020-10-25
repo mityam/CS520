207066c124f81d4bb0eae258921c0c949fedc9f9
mityamyahoo.com <mityamyahoo.com@DELL-T7810>
2020-10-25 11:59:07 -0400

    fixed type of double for strudent average

--- HW3_Morgun/src/cs520/hw3/part2/Student.java
+++ HW3_Morgun/src/cs520/hw3/part2/Student.java
@@ -29,7 +29,9 @@ public class Student {
 	//part2.1.d
 	public double computeAverage()
 	{
-		return (homework1 + homework2 + homework3 + homework4 + homework5 + homework6)/6;
+		double d = (Double.valueOf(homework1) + Double.valueOf(homework2) + Double.valueOf(homework3)
+		+ Double.valueOf(homework4) + Double.valueOf(homework5) + Double.valueOf(homework6))/6;
+		return d;
 	}
 
 	//part2.1.e
--- HW3_Morgun/src/cs520/hw3/part2/Test.java
+++ HW3_Morgun/src/cs520/hw3/part2/Test.java
@@ -4,26 +4,62 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
 	try {	
+		//part2.2.a
 		BufferedReader bufferedReader;
 		bufferedReader = new BufferedReader(new FileReader("data.txt"));
-
+		
+		//part2.2.a
+		String line;
+		while ((line = bufferedReader.readLine()) != null)
+		{
+			//part2.2.c.4
+			System.out.println(processInputData(line));
+		}
 	}
 	
 	catch (FileNotFoundException e)
 	{
+		System.out.println("File not found"); 
 		System.out.println("Exception = " + e.toString());
 	}
 	catch (IOException e)
 	{
-		
+		System.out.println("Error reading data.txt"); 
+		System.out.println("Exception = " + e.toString());
 	}
 	
 	}
 
+	
+//part2.2.c	
+private static String processInputData(String value)
+
+{
+	//part2.2.c.1
+	StringTokenizer tokenizer = new StringTokenizer(value, ",");
+	//part2.2.c.2
+	Student currentStudent = new Student(tokenizer.nextToken());
+	
+	//part2.2.c.3
+	currentStudent.setHomework1(Integer.parseInt(tokenizer.nextToken()));
+	currentStudent.setHomework2(Integer.parseInt(tokenizer.nextToken()));
+	currentStudent.setHomework3(Integer.parseInt(tokenizer.nextToken()));
+	currentStudent.setHomework4(Integer.parseInt(tokenizer.nextToken()));
+	currentStudent.setHomework5(Integer.parseInt(tokenizer.nextToken()));
+	currentStudent.setHomework6(Integer.parseInt(tokenizer.nextToken()));
+	
+	//part2.2.c.4
+	return currentStudent.toString();
+	
+}
+	
+	
+	
 }
