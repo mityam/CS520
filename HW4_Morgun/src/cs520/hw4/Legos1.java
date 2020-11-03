9258619f328398a733358d82a6c1edafdd8e24b2
mityamyahoo.com <mityamyahoo.com@DELL-T7810>
2020-11-02 21:12:19 -0500

    hmmm

--- HW4_Morgun/src/cs520/hw4/Legos1.java
+++ HW4_Morgun/src/cs520/hw4/Legos1.java
@@ -10,9 +10,7 @@ row.
 Write a class named Legos1 extending the JFrame class with the following
 specifications.
 
-c. In the constructor, specify the appropriate window title using your
-lastName and assign the above instance variables with the values 20,
-300, 50, 20, and 10, respectively.
+
 d. In the paint method, using nested loops, fill the pattern from the bottom
 row all the way to the top. Notice that the number of blocks decreases
 by one for each row. Use the baseLength variable to control the loops.
@@ -22,9 +20,6 @@ Note: For each block, use the fillRoundRect method of the Graphics object.
 Use a value of 2 for arcWidth and arcHeight.
 http://docs.oracle.com/javase/6/docs/api/java/awt/Graphics.html
 */
-	
-	
-		
 	/* a. Declare the integer type instance variables startX, startY, legoWidth,
 	and legoHeight. The first two values represent the top-left coordinates
 	of the first red block in the bottom row. The next two values are the
@@ -36,7 +31,15 @@ the number of blocks in the bottom row.
 	private int startX, startY, legoWidth, legoHeight;
 	//part1.b
 	private int baseLength;
+	/* c. In the constructor, specify the appropriate window title using your
+	lastName and assign the above instance variables with the values 20,
+	300, 50, 20, and 10, respectively. */
+	//part1.c
 	
+	Legos1 ()
+	{
+		
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
