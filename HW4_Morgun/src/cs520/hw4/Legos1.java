1f0728fc142097be844953f90e07d1eea48edcec
mityam@yahoo.com <mityam@yahoo.com>
2020-11-07 15:26:26 -0500

    Pascal Pyramid

--- HW4_Morgun/src/cs520/hw4/Legos1.java
+++ HW4_Morgun/src/cs520/hw4/Legos1.java
@@ -1,48 +1,54 @@
 package cs520.hw4;
 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 
 public class Legos1 extends JFrame{
-/*
-Create a Swing GUI application that displays the following graphics of red and blue
-lego blocks. The tower is always built from bottom row to top row, left to right in each
-row.
-Write a class named Legos1 extending the JFrame class with the following
-specifications.
-
-
-d. In the paint method, using nested loops, fill the pattern from the bottom
-row all the way to the top. Notice that the number of blocks decreases
-by one for each row. Use the baseLength variable to control the loops.
-e. In the main method, create the application object, set its size to 550 by
-325 and its visibility to true to test the above graphics.
-Note: For each block, use the fillRoundRect method of the Graphics object.
-Use a value of 2 for arcWidth and arcHeight.
-http://docs.oracle.com/javase/6/docs/api/java/awt/Graphics.html
-*/
-	/* a. Declare the integer type instance variables startX, startY, legoWidth,
-	and legoHeight. The first two values represent the top-left coordinates
-	of the first red block in the bottom row. The next two values are the
-	width and height of each block.
-	b. Declare another integer instance variable, baseLength. This represents
-the number of blocks in the bottom row.
-	*/
 	//part1.a
 	private int startX, startY, legoWidth, legoHeight;
 	//part1.b
 	private int baseLength;
-	/* c. In the constructor, specify the appropriate window title using your
-	lastName and assign the above instance variables with the values 20,
-	300, 50, 20, and 10, respectively. */
 	//part1.c
-	
 	Legos1 ()
 	{
-		
+		this.startX = 20;
+		this.startY = 300;
+		this.legoWidth = 50;
+		this.legoHeight = 20;
+		this.baseLength = 10;
 	}
+	public void paint(Graphics g) {
+		    super.paint(g);
+		    
+		int verticalBlocksNumber = this.baseLength;
+		//int horizontalShiftX = this.startX;
+		
+	    for  (int i = 1; i <= verticalBlocksNumber; i++)
+	    {	
+	    	int  horizontalShiftX = this.startX;
+	    	for (int j = 1; j <= this.baseLength; j++)
+		    {
+		    	g.setColor(( j % 2 == 0) ? Color.BLUE : Color.RED);
+		      	g.fillRoundRect(horizontalShiftX, this.startY, this.legoWidth, this.legoHeight, 5, 5);
+		      	horizontalShiftX = horizontalShiftX + this.legoWidth;
+		    }
+		    this.startY = this.startY - this.legoHeight;
+		    this.startX = this.startX + legoWidth/2;
+		    this.baseLength = this.baseLength - 1;
+	    }  
+	 }	
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 public static void main(String[] args) {
+		
+	    Legos1 lego = new Legos1();
+	    lego.setTitle("Morgun");
+	    lego.setSize(550, 325);
+	    lego.setVisible(true);
 
 	}
 
