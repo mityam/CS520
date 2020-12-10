e39f3da5145641d9a5a4e0758d11d4090da40264
mityamyahoo.com <mityamyahoo.com@DELL-T7810>
2020-12-10 14:57:17 -0500

    Container class to implement iContainer interface

--- PROJ_Morgun/src/solution/Container.java
+++ PROJ_Morgun/src/solution/Container.java
@@ -1,50 +1,25 @@
 package solution;
 
-import java.util.List;
-
 import shipping.IContainer;
-import shipping.IShip;
 
-public class Container implements IShip {
-
-	public Container() {
-		// TODO Auto-generated constructor stub
-	}
+public class Container implements IContainer {
 
 	@Override
-	public String getRegistration() {
+	public String id() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void setRegistration(String id) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void addContainer(IContainer container) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public List<IContainer> containers() {
+	public String description() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<IContainer> offload() {
+	public String destinationCity() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	public void printDetails() {
-		// TODO Auto-generated method stub
-
 	}
 
 }
